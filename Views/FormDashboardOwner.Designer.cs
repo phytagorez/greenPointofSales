1e55a174714473a5b1df7bad93992f94441abf4b
Febiola-Safira <252410102017@mail.unej.ac.id>
2026-05-14 04:26:10 +0700

    mboh ws

--- Views/FormDashboardOwner.Designer.cs
+++ Views/FormDashboardOwner.Designer.cs
@@ -28,18 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDashboardOwner));
             btnMenuKaryawan = new Button();
             btnMenuProduk = new Button();
             btnLogout = new Button();
             btnMenuKatlog = new Button();
-            button2 = new Button();
-            button1 = new Button();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
             SuspendLayout();
             // 
             // btnMenuKaryawan
@@ -50,15 +42,13 @@
             btnMenuKaryawan.FlatAppearance.MouseDownBackColor = Color.FromArgb(119, 142, 157);
             btnMenuKaryawan.FlatAppearance.MouseOverBackColor = Color.FromArgb(169, 192, 157);
             btnMenuKaryawan.FlatStyle = FlatStyle.Flat;
-            btnMenuKaryawan.Font = new Font("Georgia", 9F);
             btnMenuKaryawan.ForeColor = Color.Black;
-            btnMenuKaryawan.Location = new Point(14, 269);
+            btnMenuKaryawan.Location = new Point(36, 156);
             btnMenuKaryawan.Margin = new Padding(3, 4, 3, 4);
             btnMenuKaryawan.Name = "btnMenuKaryawan";
-            btnMenuKaryawan.Size = new Size(153, 47);
+            btnMenuKaryawan.Size = new Size(106, 38);
             btnMenuKaryawan.TabIndex = 0;
-            btnMenuKaryawan.Text = "Manajemen Karyawan";
-            btnMenuKaryawan.TextAlign = ContentAlignment.MiddleLeft;
+            btnMenuKaryawan.Text = "Karyawan";
             btnMenuKaryawan.UseVisualStyleBackColor = false;
             btnMenuKaryawan.Click += btnMenuKaryawan_Click;
             // 
@@ -70,32 +60,29 @@
             btnMenuProduk.FlatAppearance.MouseDownBackColor = Color.FromArgb(119, 142, 157);
             btnMenuProduk.FlatAppearance.MouseOverBackColor = Color.FromArgb(169, 192, 157);
             btnMenuProduk.FlatStyle = FlatStyle.Flat;
-            btnMenuProduk.Font = new Font("Georgia", 9F);
             btnMenuProduk.ForeColor = Color.Black;
-            btnMenuProduk.Location = new Point(14, 149);
+            btnMenuProduk.Location = new Point(36, 97);
             btnMenuProduk.Margin = new Padding(3, 4, 3, 4);
             btnMenuProduk.Name = "btnMenuProduk";
-            btnMenuProduk.Size = new Size(153, 32);
+            btnMenuProduk.Size = new Size(106, 38);
             btnMenuProduk.TabIndex = 1;
             btnMenuProduk.Text = "Produk";
-            btnMenuProduk.TextAlign = ContentAlignment.MiddleLeft;
             btnMenuProduk.UseVisualStyleBackColor = false;
             btnMenuProduk.Click += btnMenuProduk_Click;
             // 
             // btnLogout
             // 
-            btnLogout.BackColor = Color.FromArgb(227, 233, 207);
+            btnLogout.BackColor = Color.Beige;
             btnLogout.Cursor = Cursors.Hand;
             btnLogout.FlatAppearance.BorderSize = 0;
             btnLogout.FlatAppearance.MouseDownBackColor = Color.FromArgb(215, 235, 200);
             btnLogout.FlatAppearance.MouseOverBackColor = Color.FromArgb(235, 235, 200);
             btnLogout.FlatStyle = FlatStyle.Flat;
-            btnLogout.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnLogout.ForeColor = Color.Black;
-            btnLogout.Location = new Point(59, 678);
+            btnLogout.Location = new Point(36, 562);
             btnLogout.Margin = new Padding(3, 4, 3, 4);
             btnLogout.Name = "btnLogout";
-            btnLogout.Size = new Size(106, 29);
+            btnLogout.Size = new Size(106, 38);
             btnLogout.TabIndex = 2;
             btnLogout.Text = "Logout";
             btnLogout.UseVisualStyleBackColor = false;
@@ -109,145 +96,35 @@
             btnMenuKatlog.FlatAppearance.MouseDownBackColor = Color.FromArgb(119, 142, 157);
             btnMenuKatlog.FlatAppearance.MouseOverBackColor = Color.FromArgb(169, 192, 157);
             btnMenuKatlog.FlatStyle = FlatStyle.Flat;
-            btnMenuKatlog.Font = new Font("Georgia", 9F);
             btnMenuKatlog.ForeColor = Color.Black;
-            btnMenuKatlog.Location = new Point(14, 201);
+            btnMenuKatlog.Location = new Point(36, 202);
             btnMenuKatlog.Margin = new Padding(3, 4, 3, 4);
             btnMenuKatlog.Name = "btnMenuKatlog";
-            btnMenuKatlog.Size = new Size(153, 47);
+            btnMenuKatlog.Size = new Size(106, 38);
             btnMenuKatlog.TabIndex = 3;
-            btnMenuKatlog.Text = "Manajemen Produk";
-            btnMenuKatlog.TextAlign = ContentAlignment.MiddleLeft;
+            btnMenuKatlog.Text = "Katalog";
             btnMenuKatlog.UseVisualStyleBackColor = false;
             btnMenuKatlog.Click += btnMenuKatlog_Click;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.FromArgb(149, 172, 137);
-            button2.Cursor = Cursors.Hand;
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatAppearance.MouseDownBackColor = Color.FromArgb(119, 142, 157);
-            button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(169, 192, 157);
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Georgia", 9F);
-            button2.ForeColor = Color.Black;
-            button2.Image = Properties.Resources.Ihome;
-            button2.ImageAlign = ContentAlignment.MiddleLeft;
-            button2.Location = new Point(14, 96);
-            button2.Margin = new Padding(3, 4, 3, 4);
-            button2.Name = "button2";
-            button2.Size = new Size(153, 32);
-            button2.TabIndex = 5;
-            button2.Text = "Dashboard";
-            button2.TextImageRelation = TextImageRelation.TextAboveImage;
-            button2.UseVisualStyleBackColor = false;
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.FromArgb(149, 172, 137);
-            button1.Cursor = Cursors.Hand;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(119, 142, 157);
-            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(169, 192, 157);
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Georgia", 9F);
-            button1.ForeColor = Color.Black;
-            button1.Location = new Point(14, 338);
-            button1.Margin = new Padding(3, 4, 3, 4);
-            button1.Name = "button1";
-            button1.Size = new Size(153, 32);
-            button1.TabIndex = 6;
-            button1.Text = "Laporan";
-            button1.TextAlign = ContentAlignment.MiddleLeft;
-            button1.UseVisualStyleBackColor = false;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Lucida Bright", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = SystemColors.Window;
-            label1.Location = new Point(278, 96);
-            label1.Name = "label1";
-            label1.Size = new Size(145, 19);
-            label1.TabIndex = 7;
-            label1.Text = "Total Penjualan";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.BackColor = Color.Transparent;
-            label2.Font = new Font("Lucida Bright", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.ForeColor = SystemColors.Window;
-            label2.Location = new Point(529, 96);
-            label2.Name = "label2";
-            label2.Size = new Size(179, 19);
-            label2.TabIndex = 8;
-            label2.Text = "Total Produk Busuk";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.BackColor = Color.Transparent;
-            label3.Font = new Font("Lucida Bright", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.ForeColor = SystemColors.Window;
-            label3.Location = new Point(798, 96);
-            label3.Name = "label3";
-            label3.Size = new Size(122, 19);
-            label3.TabIndex = 9;
-            label3.Text = "Total Produk";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.BackColor = Color.Transparent;
-            label4.Font = new Font("Lucida Bright", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.ForeColor = SystemColors.Window;
-            label4.Location = new Point(1050, 96);
-            label4.Name = "label4";
-            label4.Size = new Size(162, 19);
-            label4.TabIndex = 10;
-            label4.Text = "Jumlah Transaksi";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.BackColor = Color.Transparent;
-            label5.Font = new Font("Georgia", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(59, 30);
-            label5.Name = "label5";
-            label5.Size = new Size(62, 20);
-            label5.TabIndex = 11;
-            label5.Text = "Menu";
             // 
             // FormDashboardOwner
             // 
             AutoScaleDimensions = new SizeF(120F, 120F);
             AutoScaleMode = AutoScaleMode.Dpi;
-            BackgroundImage = Properties.Resources.Dash_P_O;
+            BackgroundImage = Properties.Resources.Dashboard_Ow;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(1280, 720);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(button1);
-            Controls.Add(button2);
+            ClientSize = new Size(800, 600);
             Controls.Add(btnMenuKatlog);
             Controls.Add(btnLogout);
             Controls.Add(btnMenuProduk);
             Controls.Add(btnMenuKaryawan);
             DoubleBuffered = true;
-            Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Font = new Font("Poppins", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             FormBorderStyle = FormBorderStyle.None;
-            Icon = (Icon)resources.GetObject("$this.Icon");
             Margin = new Padding(3, 4, 3, 4);
             Name = "FormDashboardOwner";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "FormDashboardOwner";
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -256,12 +133,5 @@
         private Button btnMenuProduk;
         private Button btnLogout;
         private Button btnMenuKatlog;
-        private Button button2;
-        private Button button1;
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
     }
 }
